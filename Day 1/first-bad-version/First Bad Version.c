721b7a25e5f357847004dd9e740e57679046f96c
Abdullah Hanfy <abdallahhanfy@outlook.com>
2021-09-28 18:11:14 +0200

    Add first bad version function

---
+++ Day 1/first-bad-version/First Bad Version.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : First.c
+ Author      : Abdullah Hanfy
+ Version     :
+ Copyright   : Your copyright notice
+ Description : First Bad Version
+ ============================================================================
+ *You are a product manager and currently leading a team to develop a new product.
+ *Unfortunately, the latest version of your product fails the quality check.
+ *Since each version is developed based on the previous version,
+ *all the versions after a bad version are also bad.
+ *Suppose you have n versions [1, 2, ..., n]
+ *and you want to find out the first bad one,
+ *which causes all the following ones to be bad.
+ *
+ *You are given an API bool isBadVersion(version) which returns whether version is bad.
+ *Implement a function to find the first bad version.
+ *You should minimize the number of calls to the API.
+ *
+ */
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    long low =1 ;
+    long high = n ;
+
+
+    while(low<high)
+    {
+        long mid = (low + high ) /2 ;
+        if(isBadVersion(mid))
+        {
+             high = mid;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+
+    return low;
+
+}
